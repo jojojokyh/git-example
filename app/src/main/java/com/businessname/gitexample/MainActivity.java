4e48cdc435548239c7f53eb2cc0bf1e8002a8586
Jo <joanne.yager@student.kyh.se>
2021-09-15 13:23:33 +0200

    test commit

---
+++ app/src/main/java/com/businessname/gitexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.businessname.gitexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int testCommit = 100;
+        int testBranch = 200;
+    }
+}
